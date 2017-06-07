08baa0122607a89a5f11962462883466b54d361a
D <s90789@outlook.com>
2017-06-07 22:49:50 +0800

    Add sort util

---
+++ app/src/main/java/com/d/uiutil/SortActivity.java
@@ -0,0 +1,72 @@
+package com.d.uiutil;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.d.uiutil.sort.SideBar;
+import com.d.uiutil.sort.SortAdapter;
+import com.d.uiutil.sort.SortBean;
+import com.d.uiutil.sort.SortUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortActivity extends AppCompatActivity {
+    private LinearLayout llytTin;
+    private TextView tvTinLetter;
+    private RecyclerView rvList;
+    private SideBar sideBar;
+    private SortUtil sortUtil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort);
+        llytTin = (LinearLayout) findViewById(R.id.llyt_tin);
+        tvTinLetter = (TextView) findViewById(R.id.tv_tin_letter);
+        rvList = (RecyclerView) findViewById(R.id.rv_list);
+        sideBar = (SideBar) findViewById(R.id.sb_sidebar);
+        init();
+    }
+
+    private void init() {
+        List<SortBean> datas = getDatas();
+        sortUtil = new SortUtil();
+        sortUtil.sortDatas(datas);
+        rvList.setLayoutManager(new LinearLayoutManager(this));
+        rvList.setAdapter(new SortAdapter(this, datas, R.layout.adapter_sort));
+        rvList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                sortUtil.onScrolled(recyclerView, llytTin, tvTinLetter);
+            }
+        });
+        sideBar.setOnLetterChangedListener(new SideBar.OnLetterChangedListener() {
+            @Override
+            public void onChange(int index, String c) {
+                sortUtil.onChange(index, c, rvList);
+            }
+        });
+    }
+
+    public List<SortBean> getDatas() {
+        String[] arrays = getResources().getStringArray(R.array.date);
+        List<SortBean> datas = new ArrayList<>();
+        for (String c : arrays) {
+            SortBean bean = new SortBean(c);
+            bean.content = c;
+            datas.add(bean);
+        }
+        return datas;
+    }
+}
---
+++ app/src/main/java/com/d/uiutil/sort/SortUtil.java
@@ -0,0 +1,122 @@
+package com.d.uiutil.sort;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.github.promeg.pinyinhelper.Pinyin;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SortUtil
+ * Created by D on 2017/6/7.
+ */
+public class SortUtil {
+    private List<SortBean> datas;
+    private Map<String, int[]> letterMap;
+    private int lastFirstVisibleItem = -1;
+
+    public void onScrolled(RecyclerView recyclerView, View layout, TextView tvLetter) {
+        if (recyclerView == null || layout == null || tvLetter == null || letterMap == null || datas == null) {
+            return;
+        }
+        RecyclerView.LayoutManager manager = recyclerView.getLayoutManager();
+        if (manager == null || !(manager instanceof LinearLayoutManager)) {
+            return;
+        }
+        int firstVisibleItemPosition = ((LinearLayoutManager) manager).findFirstVisibleItemPosition();
+
+        int[] value = letterMap.get(datas.get(firstVisibleItemPosition).letter);
+        int nextSectionPosition = value != null ? value[1] : -1;
+        if (firstVisibleItemPosition != lastFirstVisibleItem) {
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layout.getLayoutParams();
+            params.topMargin = 0;
+            layout.setLayoutParams(params);
+            tvLetter.setText(datas.get(firstVisibleItemPosition).letter);
+        }
+        if (nextSectionPosition == firstVisibleItemPosition + 1) {
+            View childView = recyclerView.getChildAt(0);
+            if (childView != null) {
+                int titleHeight = layout.getHeight();
+                int bottom = childView.getBottom();
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) layout.getLayoutParams();
+                if (bottom < titleHeight) {
+                    float pushedDistance = bottom - titleHeight;
+                    params.topMargin = (int) pushedDistance;
+                    layout.setLayoutParams(params);
+                } else {
+                    if (params.topMargin != 0) {
+                        params.topMargin = 0;
+                        layout.setLayoutParams(params);
+                    }
+                }
+            }
+        }
+        lastFirstVisibleItem = firstVisibleItemPosition;
+    }
+
+    public void onChange(int index, String c, RecyclerView recyclerView) {
+        if (recyclerView == null || letterMap == null) {
+            return;
+        }
+        RecyclerView.LayoutManager lm = recyclerView.getLayoutManager();
+        if (lm == null || !(lm instanceof LinearLayoutManager)) {
+            return;
+        }
+        LinearLayoutManager manager = (LinearLayoutManager) lm;
+        if (index == 0) {
+            manager.scrollToPositionWithOffset(0, 0);//置顶
+            return;
+        }
+        int[] value = letterMap.get(c);
+        if (value != null) {
+            manager.scrollToPositionWithOffset(value[0], 0);
+        }
+    }
+
+    public Map<String, int[]> sortDatas(List<SortBean> list) {
+        letterMap = new HashMap<>();
+        if (list == null || list.size() <= 0) {
+            return letterMap;
+        }
+        int count = list.size();
+        for (int i = 0; i < count; i++) {
+            SortBean bean = list.get(i);
+            String pinyin = Pinyin.toPinyin(bean.content, "");
+            String letter = TextUtils.isEmpty(pinyin) ? "" : pinyin.substring(0, 1).toUpperCase();
+            //正则表达式，判断首字母是否是英文字母/数字/其他
+            if (letter.matches("[0-9]")) {
+                letter = "☆";
+            } else if (!letter.matches("[A-Z]")) {
+                letter = "#";
+            }
+            bean.pinyin = pinyin;
+            bean.letter = letter;
+            bean.isLetter = false;
+        }
+        Collections.sort(list, new PinyinComparator());
+        String key = null;
+        int[] value = null;//is[0]:thisSectionPosition,is[1]:nextSectionPosition
+        for (int i = 0; i < count; i++) {
+            SortBean b = list.get(i);
+            if (!TextUtils.equals(key, b.letter)) {
+                key = b.letter;
+                b.isLetter = true;
+                if (value != null) {
+                    value[1] = i;
+                }
+                value = new int[]{i, -1};
+                letterMap.put(key, value);
+            }
+        }
+        datas = list;
+        return letterMap;
+    }
+}
