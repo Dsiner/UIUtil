cb4038d7dd9f6c84f7b793dbd4e53545b79c28f8
D <s90789@outlook.com>
2018-08-10 09:48:24 +0800

    TouchEvent

--- lib_ui/src/main/java/com/d/lib/ui/common/touchevent/ViewF.java
+++ lib_ui/src/main/java/com/d/lib/ui/common/touchevent/ViewF.java
@@ -3,13 +3,65 @@ package com.d.lib.ui.common.touchevent;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewDebug;
 
 /**
- * ViewF
+ * ViewF - Gingerbread - 2.3.7
  * Created by D on 2017/5/30.
  */
 public class ViewF extends View {
+
+    int mPrivateFlags;
+
+    /**
+     * The view flags hold various views states.
+     * {@hide}
+     */
+    @ViewDebug.ExportedProperty
+    int mViewFlags;
+
+    /**
+     * Mask for use with setFlags indicating bits used for indicating whether
+     * this view is enabled
+     * {@hide}
+     */
+    static final int ENABLED_MASK = 0x00000020;
+
+    /**
+     * This view is enabled. Intrepretation varies by subclass.
+     * Use with ENABLED_MASK when calling setFlags.
+     * {@hide}
+     */
+    static final int ENABLED = 0x00000000;
+
+    /**
+     * This view is disabled. Intrepretation varies by subclass.
+     * Use with ENABLED_MASK when calling setFlags.
+     * {@hide}
+     */
+    static final int DISABLED = 0x00000020;
+
+    /**
+     * <p>Indicates this view can be clicked. When clickable, a View reacts
+     * to clicks by notifying the OnClickListener.<p>
+     * {@hide}
+     */
+    static final int CLICKABLE = 0x00004000;
+
+    /**
+     * <p>
+     * Indicates this view can be long clicked. When long clickable, a View
+     * reacts to long clicks by notifying the OnLongClickListener or showing a
+     * context menu.
+     * </p>
+     * {@hide}
+     */
+    static final int LONG_CLICKABLE = 0x00200000;
+
+    private OnTouchListener mOnTouchListener;
+
     public ViewF(Context context) {
         super(context);
     }
@@ -22,34 +74,43 @@ public class ViewF extends View {
         super(context, attrs, defStyleAttr);
     }
 
-//    /**
-//     * @return onTouchEvent(event)
-//     */
-//    @Override
-//    public boolean dispatchTouchEvent(MotionEvent event) {
-//        if (mOnTouchListener != null && (mViewFlags & ENABLED_MASK) == ENABLED &&
-//                mOnTouchListener.onTouch(this, event)) {
-//            return true;
-//        }
-//        return onTouchEvent(event);
-//    }
-//
-//    /**
-//     * @return false
-//     */
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        final int viewFlags = mViewFlags;
-//        if ((viewFlags & ENABLED_MASK) == DISABLED) {
-//            // A disabled view that is clickable still consumes the touch
-//            // events, it just doesn't respond to them.
-//            return (((viewFlags & CLICKABLE) == CLICKABLE ||
-//                    (viewFlags & LONG_CLICKABLE) == LONG_CLICKABLE));
-//        }
-//        if (((viewFlags & CLICKABLE) == CLICKABLE ||
-//                (viewFlags & LONG_CLICKABLE) == LONG_CLICKABLE)) {
-//            return true;
-//        }
-//        return false;
-//    }
+    /**
+     * Pass the touch screen motion event down to the target view, or this
+     * view if it is the target.
+     *
+     * @param event The motion event to be dispatched.
+     * @return True if the event was handled by the view, false otherwise.
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (mOnTouchListener != null && (mViewFlags & ENABLED_MASK) == ENABLED &&
+                mOnTouchListener.onTouch(this, event)) {
+            return true;
+        }
+        return onTouchEvent(event);
+    }
+
+    /**
+     * Implement this method to handle touch screen motion events.
+     *
+     * @param event The motion event.
+     * @return True if the event was handled, false otherwise.
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final int viewFlags = mViewFlags;
+
+        if ((viewFlags & ENABLED_MASK) == DISABLED) {
+            // A disabled view that is clickable still consumes the touch
+            // events, it just doesn't respond to them.
+            return (((viewFlags & CLICKABLE) == CLICKABLE ||
+                    (viewFlags & LONG_CLICKABLE) == LONG_CLICKABLE));
+        }
+
+        if (((viewFlags & CLICKABLE) == CLICKABLE ||
+                (viewFlags & LONG_CLICKABLE) == LONG_CLICKABLE)) {
+            return true;
+        }
+        return false;
+    }
 }
